62def686a7d0e3c6b923700266f3a3eaff8cfd2a
komal chaudhary <chaudharykomal681@gmail.com>
2025-09-29 12:03:40 +0530

    54 spiral matrix.cpp

---
+++ Array/54 spiral matrix.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> res;
+    if (matrix.empty()) return res;
+
+    int n = matrix.size(), m = matrix[0].size();
+    int top = 0, bottom = n-1, left = 0, right = m-1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) res.push_back(matrix[top][i]);
+        top++;
+        for (int i = top; i <= bottom; i++) res.push_back(matrix[i][right]);
+        right--;
+        if (top <= bottom)
+            for (int i = right; i >= left; i--) res.push_back(matrix[bottom][i]);
+        bottom--;
+        if (left <= right)
+            for (int i = bottom; i >= top; i--) res.push_back(matrix[i][left]);
+        left++;
+    }
+
+    return res;
+}
+
+int main() {
+    vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<int> ans = spiralOrder(mat);
+    for (int x : ans) cout << x << " ";
+}
